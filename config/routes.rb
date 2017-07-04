71597f9ae42137ddbab159814f24316ba6cc11c1
Ilya-R <megabonus777@mail.ru>
2017-07-05 01:32:21 +1000

    отредактировал routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
- #root 'users#show'
-  resources :users, only: [:show, :edit, :update]
-  resources :books
+
+  root 'books#main'
+
+  resources :users, only: %i[show edit update] do
+    delete '/book_users/:id', to: 'book_users#destroy'
+  end
+
+  resources :books do
+    resources :comments, only: %i[create destroy]
+    post '/book_user_ratings', to: 'book_user_ratings#create'
+    resources :book_users, only: %i[create destroy]
+  end
 end
