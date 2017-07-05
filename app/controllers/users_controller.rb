abc55734fb42c7c9e4c5ec9a76881be062e679a0
Ilya-R <megabonus777@mail.ru>
2017-07-06 00:20:52 +1000

    Дописан контроллер пользователя

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,30 @@ class UsersController < ApplicationController
   # задаем объект @user для шаблонов и экшенов
   before_action :set_current_user, except: [:show]
 
-  def show; end
+  # GET /users/1
+  def show
+    @user = User.find(params[:id])
+  end
 
   def edit; end
 
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to @user, notice: 'Данные пользователя были успешно обновлены'
     else
       render :edit
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_current_user
-      @user = current_user
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_current_user
+    @user = current_user
+  end
 
-    def user_params
-      params[:user]
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :nickname, :age)
+  end
+
 end
+
